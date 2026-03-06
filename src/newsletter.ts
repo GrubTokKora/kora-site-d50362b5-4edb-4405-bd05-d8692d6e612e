b0bbc6c3e6509bbc907d7756ffd17b7eb24cde65
kysz-mann <mann.prajapati@kysz.tech>
2026-03-06 11:48:06 +0530

    Update newsletter.ts

--- src/newsletter.ts
+++ src/newsletter.ts
@@ -56,7 +56,7 @@ export async function subscribeToNewsletter(
     source: 'dynamic_website_widget',
   }
 
-  const resp = await fetch(`https://kora-agent.quseappdev.com/api/public/newsletter/subscribe`, {
+  const resp = await fetch(`https://kora-agent.quseappdev.com/api/v1/public/newsletter/subscribe`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
